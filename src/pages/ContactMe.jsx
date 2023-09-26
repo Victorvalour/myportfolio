76cf340d06e7c6c066897f3de2deaf4cc3a10a54
Victorvalour <ewehvictor7@gmail.com>
2023-09-26 19:52:25 +0100

    update

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const ContactMe = () => {
   return (
-    <div className="pt-10 px-3">
+    <div className="pt-10 h-screen bg-violet-900 px-3 flex flex-col gap-10">
         <div className="flex justify-between">
       <h1 className="text-4xl font-bold">Contact Me</h1>
 
@@ -13,6 +13,14 @@ const ContactMe = () => {
       </Link>
       </div>
 
+        <div className="font-bold ml-8">
+           <p className="text-black text-2xl mb-8"> Email: <a href="mailto:ewehvictor7@gmail.com" className="underline text-white">ewehvictor7@gmail.com</a> </p>
+           <p className="text-black text-2xl mb-8">Phone: <span className="text-white"> +234 806 067 8108 </span></p>
+
+           <p  className="text-black text-2xl mb-8">
+            Twitter: <a href="https://twitter.com/victor_valour" className="text-white underline">Eweh Victor E.</a>
+           </p>
+        </div>
 
     </div>
   );
